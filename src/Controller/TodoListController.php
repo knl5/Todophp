98edbcab08810f4ad2317f5eef8996ae46200171
Kanelle Lorent <kanelle.lorent@mail-ecv.fr>
2022-01-06 17:27:15 +0100

    add third route

--- src/Controller/TodoListController.php
+++ src/Controller/TodoListController.php
@@ -10,31 +10,36 @@ use Symfony\Component\Routing\Annotation\Route;
 class TodoListController extends AbstractController
 {
     public static $session_key ='todolist';
-    /* @Route('/', name: 'todolist_list') */
+
+    #[Route('/', name: 'todolist_list')]
     public function list(RequestStack $requestStack): Response
     {
-        $session = requestStack->getSession();
+        $session = $requestStack->getSession();
         return $this->render('todolist/index.html.twig', [
             'list' => $session -> get(self::$session_key),
         ]);
     }
 
-    /* @Route('/add', name: 'todolist_add', methods={"POST"}) */
+    #[Route('/add', name: 'add')]
     public function add(RequestStack $requestStack): Response
     {
         $session = $requestStack->getSession();
         $list = $session->get(self::$session_key);
-        $list[] = $requestStack->getMainRequest()->request->get('value');
+
+
+        $list[] = $requestStack->getMainRequest()->request->get('addItem');
         $session->set(self::$session_key, $list);
 
         return $this->redirectToRoute('todolist_list');
     }
 
-    /* #[Route('/delete', name: 'todolist')]
+    #[Route('/delete', name: 'delete')]
     public function delete(RequestStack $requestStack): Response
     {
-        return $this->render('conference/index.html.twig', [
-            'controller_name' => 'TodoListController',
-        ]);
-    } */
+        $session = $requestStack->getSession();
+        $list = $session->get(self::$session_key);
+        $removeElement = $requestStack->getMainRequest()->query->get('remove');
+        unset($list[removeElement]);
+        $new_list = [];
+    } 
 }
