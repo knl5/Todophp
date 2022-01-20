dd0cbe86c60a4efe55b55ce12a47957c32df6de9
Kanelle Lorent <kanelle.lorent@mail-ecv.fr>
2022-01-20 14:33:04 +0100

    finish project

--- src/Controller/TodoListController.php
+++ src/Controller/TodoListController.php
@@ -15,31 +15,48 @@ class TodoListController extends AbstractController
     public function list(RequestStack $requestStack): Response
     {
         $session = $requestStack->getSession();
-        return $this->render('todolist/index.html.twig', [
-            'list' => $session -> get(self::$session_key),
+
+        $list =$session->get('todolist');
+
+        $template = $this->render('todolist/index.html.twig', [
+            'list' => $list,
         ]);
+
+        return $template;
+        /* return $this->render('todolist/index.html.twig', [
+            'list' => $session -> get(self::$session_key),
+        ]); */
     }
 
-    #[Route('/add', name: 'add')]
+    
+    #[Route('/add', name: 'todolist_add')]
     public function add(RequestStack $requestStack): Response
     {
-        $session = $requestStack->getSession();
+        $request = $requestStack->getMainRequest();
+        $session = $requestStack->getSession();/* getSession(); */
+
         $list = $session->get(self::$session_key);
-
-
-        $list[] = $requestStack->getMainRequest()->request->get('addItem');
-        $session->set(self::$session_key, $list);
+        $list[] = $request->query->get('value'); /* Stack->getMainRequest()->request->get('addItem'); */
+        
+        $session->set('todolist', $list);
 
         return $this->redirectToRoute('todolist_list');
     }
 
-    #[Route('/delete', name: 'delete')]
-    public function delete(RequestStack $requestStack): Response
+    
+    #[Route('/delete/{$id}', name: 'todolist_delete')]
+    public function delete(RequestStack $requestStack, $id): Response
     {
         $session = $requestStack->getSession();
-        $list = $session->get(self::$session_key);
-        $removeElement = $requestStack->getMainRequest()->query->get('remove');
+        $list = $session->get('todoList');/* (self::$session_key) */;
+        $list = array_values($list);
+        unset($list[$id]);
+        
+        $session->set('todolist', $list);
+
+        return $this->redirectToRoute('todolist_list');
+        /* $removeElement = $requestStack->getMainRequest()->query->get('remove');
         unset($list[removeElement]);
-        $new_list = [];
+        $new_list = []; */
     } 
 }
